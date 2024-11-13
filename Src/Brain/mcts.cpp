164bd91480fd17652e14c619129227e729701c31
Alea Chlodnik <achlodnik@gmail.com>
2024-11-13 18:52:44 +0100

    [DEL] unnecessary comments

--- Src/Brain/mcts.cpp
+++ Src/Brain/mcts.cpp
@@ -8,54 +8,6 @@
 #include <iostream>
 #include "mcts.hpp"
 
-///////////////////////////////////////////////////////////////////////////////////////////
-std::ostream &operator<<(std::ostream &os, const TILE_STATE entry)
-{
-  switch (entry) {
-  case TILE_STATE::EMPTY:
-    os << "X";
-    break;
-  case TILE_STATE::ME:
-    os << "1";
-    break;
-  case TILE_STATE::PLAYER2:
-    os << "2";
-    break;
-  default:
-    os << "UNDEFINED";
-    break;
-  }
-  return os;
-}
-void displayCurrGame(gomoku_t *_game)
-{
-  int space = 1;
-  for (int i = _game->size.y; i > 9; i /= 10)
-    space++;
-
-  auto placeSpaceHeight = [](int x, int nbr) {
-    int remove_space = 0;
-    for (int i = nbr; i > 9; i /= 10)
-      remove_space++;
-    x -= remove_space;
-    for (int i = 0; i < x; i++)
-      std::cout << " ";
-  };
-
-  std::cout << std::endl;
-  for (int i = 0; i < _game->size.y; i++) {
-    std::cout << i;
-    placeSpaceHeight(space, i);
-    std::cout << "| ";
-    for (int j = 0; j < _game->size.x; j++) {
-      std::cout << _game->map[i][j] << " ";
-    }
-    std::cout << std::endl;
-  }
-  std::cout << std::endl;
-}
-/////////////////////////////////////////////////////////////////////////////////////////////
-
 Node *MCTS::select(Node *node)
 {
     // while (!node->isTerminal() && node->isFullyExpanded())
@@ -87,28 +39,16 @@ int MCTS::simulate(Node *node)
     }
     std::pair<int, int> move;
     std::vector<std::pair<int, int>> availableMovesMe = _gameLogic.getAvailableAdjacentMoves(simulatedState, TILE_STATE::ME);
-    // std::vector<std::pair<int, int>> availableMovesOpponent = _gameLogic.getAvailableAdjacentMoves(simulatedState, TILE_STATE::PLAYER2);
     while (simulatedState.state == GAME_STATE::PLAY)
     {
         if (availableMovesMe.empty())
         {
             availableMovesMe = _gameLogic.getAvailableMoves(simulatedState);
         }
-        // if (availableMovesOpponent.empty())
-        // {
-        //     availableMovesOpponent = _gameLogic.getAvailableMoves(simulatedState);
-        // }
 
-        if (isCurrentPlayer)
-        {
-            move = availableMovesMe[rand() % availableMovesMe.size()];
-        }
-        // else
-        // {
-        //     move = availableMovesOpponent[rand() % availableMovesOpponent.size()];
-        // }
+        move = availableMovesMe[rand() % availableMovesMe.size()];
+
         simulatedState.map[move.first][move.second] = isCurrentPlayer ? TILE_STATE::ME : TILE_STATE::PLAYER2;
-        // simulatedState.my_turn = !isCurrentPlayer;
 
         if (isCurrentPlayer && _gameLogic.checkWin(simulatedState))
         {
@@ -124,7 +64,6 @@ int MCTS::simulate(Node *node)
 
         isCurrentPlayer = !isCurrentPlayer;
         availableMovesMe = _gameLogic.getAvailableAdjacentMoves(simulatedState, TILE_STATE::ME);
-        // availableMovesOpponent = _gameLogic.getAvailableAdjacentMoves(simulatedState, TILE_STATE::PLAYER2);
 
         depth++;
     }
@@ -147,28 +86,21 @@ void MCTS::backPropagate(Node *node, int depth)
 
 Node *MCTS::getBestChildInfo(gomoku_t &game)
 {
-    //std::cout << "root move: " << game.me.x << " " << game.me.y << std::endl; ////////////////////
     _root = std::make_unique<Node>(game, nullptr, game.me);
-    // std::vector<std::pair<int, int>> testMoves = _gameLogic.getAvailableAdjacentMoves(game, TILE_STATE::ME);
     std::vector<std::pair<int, int>> testMoves = _gameLogic.getAvailableAdjacentMoves(game, TILE_STATE::ME);
 
-    // if (testMoves.empty())
-    // {
-    //     testMoves = _gameLogic.getAvailableMoves(game);
-    //     std::pair<int, int> move = testMoves[rand() % testMoves.size()];
-    //     coord_t moveCoord = {move.first, move.second};
-    //     auto node = std::make_unique<Node>(game, nullptr, moveCoord);
-    //     node->setFirstMove(move);
-    //     return node.get();
-    // }
-
-    // for (auto &move : testMoves) {
-    //     std::cout << "test move: " << move.first << " " << move.second << std::endl; ////////////////////
-    // }
+    if (testMoves.empty())
+    {
+        testMoves = _gameLogic.getAvailableMoves(game);
+        std::pair<int, int> move = testMoves[rand() % testMoves.size()];
+        coord_t moveCoord = {move.first, move.second};
+        auto node = std::make_unique<Node>(game, nullptr, moveCoord);
+        node->setFirstMove(move);
+        return node.get();
+    }
 
     for (auto &move : testMoves)
     {
-        //std::cout << "test move: " << move.first << " " << move.second << std::endl; ////////////////////
         if (game.me.x == move.first && game.me.y == move.second)
         {
             continue;
@@ -176,8 +108,6 @@ Node *MCTS::getBestChildInfo(gomoku_t &game)
         Node *child = _root->expand(move);
         child->setFirstMove(move);
         int depth = simulate(child);
-        // std::cout << "test move: " << move.first << " " << move.second << std::endl; ////////////////////
-        // std::cout << "depth: " << depth << std::endl; ////////////////////
         backPropagate(child, depth);
     }
     
