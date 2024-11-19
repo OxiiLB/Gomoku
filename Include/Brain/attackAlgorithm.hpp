a6dea5152531bd73fbcb9e2805cde1c0072d1cb5
Marc <marc.mendia@epitech.eu>
2024-11-19 14:22:49 +0100

    [ADD] file remove  by error

---
+++ Include/Brain/attackAlgorithm.hpp
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2024
+** Gomoku
+** File description:
+** Attack algorithm
+*/
+
+#pragma once
+
+#include <random>
+#include <chrono>
+#include <ctime>
+#include <unordered_map>
+#include "struct.hpp"
+#include "enum.hpp"
+
+enum class Direction {
+    HORIZONTAL,
+    VERTICAL,
+    LEFT_DIAGONAL,
+    RIGHT_DIAGONAL
+};
+
+struct RowInfo {
+    int len = 0;
+    Direction direction;
+    bool positive = true;
+    std::pair<int, int> startPos;
+    std::pair<int, int> endPos;
+};
+
+class AttackAlgorithm {
+    public:
+        AttackAlgorithm(const gomoku_t &game) {}
+        ~AttackAlgorithm() {}
+
+        std::vector<RowInfo> getRows(gomoku_t &game);
+        std::vector<std::pair<int, int>> getAvailableMoves(const gomoku_t &game);
+        std::unordered_map<int, std::pair<int, int>> getBestMoveInfo(gomoku_t &game);
+        bool canWin(gomoku_t &gameState, RowInfo *row, std::pair<int, int> pos, int depth);
+        std::unordered_map<int, std::pair<int, int>> getBestMove(gomoku_t &game, std::vector<RowInfo> rows);
+};
---
+++ Include/Brain/brain.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** Gomoku
+** File description:
+** brain
+*/
+
+#pragma once
+
+#include "struct.hpp"
+#include "attackAlgorithm.hpp"
+
+class Brain {
+public:
+    Brain() {}
+
+    void getBestAttackMove(gomoku_t *game);
+};
